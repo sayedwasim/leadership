07ca588e3eca0a891fb17d7c21c9e9d5cff6d714
sayedwasim <wasimalhaddad@gmail.com>
2022-01-02 22:01:32 +0300

    files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	/*---------- LEADERSHIP ----------*/
+	$('[data-fancybox="profile"]').fancybox({
+		type: "ajax",
+		src: "profiles.html",
+		animationEffect: "fade",
+		transitionEffect:"circular"
+	});
+	
+});//End of document.ready
